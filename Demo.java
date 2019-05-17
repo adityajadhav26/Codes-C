59008bbc5332ea023efd4aaa4164c32cd8875f8a
Aditya Jadhav <adityabjadhav8@gmail.com>
2019-05-17 21:30:14 +0530

    adding downcasting in code
    object downcasting

--- Demo.java
+++ Demo.java
@@ -5,6 +5,9 @@ class Demo{
 	public static void main(String []args){
 	A a = new B(); //upcasting
 	I i = new B(); // upcasting
-	Object o = new B(); // upcasting 
+	Object o = new B(); // upcasting
+
+	 A obj = new A();
+	 B b = (B)obj;// downcasting
 	}
 }
